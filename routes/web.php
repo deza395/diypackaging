df47f6df84d9eaab11a30890900383b39dd59346
deza395 <debora.ed.zarate@gmail.com>
2019-07-14 16:30:42 -0300

    proyecto en laravel

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// creo las rutas que vamos a usar para el proyecto, por el momento estan vacias
+// ruta del index
+Route::get('/index', function () {
+    return view('');
+});
+
+// ruta productos
+Route::get('/productos', function () {
+    return view('');
+});
+// ruta diseños
+Route::get('/personalizados', function () {
+    return view('');
+});
+// ruta accesorios
+Route::get('/accesorios', function () {
+    return view('');
+});
+// ruta FAQ
+Route::get('/faq', function () {
+    return view('');
+});
+// ruta Iniciar sesion
+Route::get('/ingresar', function () {
+    return view('');
+});
+// ruta registro
+Route::get('/registro', function () {
+    return view('');
+});
+// ruta perfil
+Route::get('/perfil', function () {
+    return view('');
+});
+// ruta carrito
+Route::get('/carrito', function () {
+    return view('');
+});
