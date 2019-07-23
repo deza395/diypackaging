a1dfd411b509d110144ef60d75d6ae98fdb94b69
deza395 <debora.ed.zarate@gmail.com>
2019-07-23 20:28:22 -0300

    modificaciones en las vistas

--- routes/web.php
+++ routes/web.php
@@ -14,37 +14,37 @@
 // ruta del index
 Route::get('/index', function () {
     return view('');
-});
-
-// ruta productos
-Route::get('/productos', function () {
-    return view('');
-});
-// ruta diseños
-Route::get('/personalizados', function () {
-    return view('');
-});
-// ruta accesorios
-Route::get('/accesorios', function () {
-    return view('');
-});
-// ruta FAQ
-Route::get('/faq', function () {
-    return view('');
-});
-// ruta Iniciar sesion
-Route::get('/ingresar', function () {
-    return view('');
-});
-// ruta registro
-Route::get('/registro', function () {
-    return view('');
-});
-// ruta perfil
-Route::get('/perfil', function () {
-    return view('');
-});
-// ruta carrito
-Route::get('/carrito', function () {
-    return view('');
-});
+// });
+//
+// // ruta productos
+// Route::get('/productos', function () {
+//     return view('');
+// });
+// // ruta diseños
+// Route::get('/personalizados', function () {
+//     return view('');
+// });
+// // ruta accesorios
+// Route::get('/accesorios', function () {
+//     return view('');
+// });
+// // ruta FAQ
+// Route::get('/faq', function () {
+//     return view('');
+// });
+// // ruta Iniciar sesion
+// Route::get('/ingresar', function () {
+//     return view('');
+// });
+// // ruta registro
+// Route::get('/registro', function () {
+//     return view('');
+// });
+// // ruta perfil
+// Route::get('/perfil', function () {
+//     return view('');
+// });
+// // ruta carrito
+// Route::get('/carrito', function () {
+//     return view('');
+// });
